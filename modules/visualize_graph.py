329a971d75a43e67226b9b466895841425ba7cd9
sharadshriram <sharadshriram01@gmail.com>
2020-01-01 19:58:32 +0530

    graphviz for plotting on notebooks

--- modules/visualize_graph.py
+++ modules/visualize_graph.py
@@ -1,43 +1,46 @@
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+import os
+import re
+import fileinput
 import numpy as np
-import networkx as nx
+import graphviz
 
 def animateGraph(num):
   pass
 
 
-def displayPath(graph, edges):
-    f, ax = plt.subplots(figsize=(8,6))
-    layout = nx.shell_layout(graph)
-    labels = nx.get_edge_attributes(graph, 'weight')
-    nx.draw_networkx_nodes(graph, pos=layout, node_size=700, node_color="gray") #place the nodes
-    nx.draw_networkx_edges(graph, pos=layout, alpha=0.3, width=0.5)
-    nx.draw_networkx_edges(graph, pos=layout, edgelist=edges, width=4, edge_color='r', style="dashed")
+def displayPath(edgeList):
+  savedGraph = os.getcwd() + os.path.abspath("/modules/savedGraph.gv")
 
-    nx.draw_networkx_labels(graph, pos=layout, font_size=20)
-    nx.draw_networkx_edge_labels(graph, pos=layout, edge_labels=labels, font_size=20)
+  f = open(savedGraph, "r")
+  data = f.readlines()
+  f.close()
+  g = open(savedGraph, "w+")
+  for index, line in enumerate(data):
+    edgeLine = ""
+    
+    if re.findall("\w -- \w", line) and edgeLine == "":
+      for (u, v, w) in edgeList:
+        edge = line.strip().split()  #['\t0', '--', '1', '[label=1]\n']
+        if (int(edge[0]) == u and int(edge[2]) == v) or (int(edge[0]) == v and int(edge[2]) == u) :
+          edge[3] = edge[3].replace("]", ", color=red, penwidth=3]")
+          edgeLine = "\t"+ edge[0] + " " + edge[1] + " " + edge[2]+ " " +edge[3] +"\n"
+          line = edgeLine
 
-    ax.axis('off')
-    return ax
+    g.write(line)
+  g.close()
+
+  return graphviz.Source.from_file(savedGraph)
 
 
-def visualizeGraph(adjMatrix, nodeSet=None):
-    graph = nx.from_numpy_matrix(np.matrix(adjMatrix), create_using=nx.Graph)
-    layout = nx.shell_layout(graph)
-    labels = nx.get_edge_attributes(graph, 'weight')
-    nx.draw(graph, layout, node_size=800, with_labels=True)
-    nx.draw_networkx_edge_labels(graph, pos=layout, edge_labels=labels)
-    plt.axis('off')
-    plt.show()
-    return graph
+def visualizeGraph(graph):
+    if graph.directed != True:
+      visualGraph = graphviz.Graph('G',  node_attr={'shape': 'circle', 'color': 'lightblue2', 'style': 'filled'})
+    else:
+      visualGraph = graphviz.Digraph('G',  node_attr={'shape': 'circle', 'color': 'lightblue2', 'style': 'filled'})
 
-def visualizeDiGraph(adjMatrix, nodeSet=None):
-    graph = nx.from_numpy_matrix(np.matrix(adjMatrix), create_using=nx.DiGraph)
-    layout = nx.shell_layout(graph)
-    labels = nx.get_edge_attributes(graph, 'weight')
-    nx.draw(graph, layout, node_size=800, with_labels=True)
-    nx.draw_networkx_edge_labels(graph, pos=layout, edge_labels=labels)
-    plt.axis('off')
-    plt.show()
-    return graph
+    for gNode in graph.get_allNodes():
+      visualGraph.node(str(gNode.index), str(gNode.index))
+    for (u, v, w) in graph.get_allEdges():
+      visualGraph.edge(str(u), str(v), label=str(w))
+    visualGraph.save(filename=os.path.abspath(os.getcwd() + "/modules/savedGraph.gv"))
+    return graphviz.Source(visualGraph)
